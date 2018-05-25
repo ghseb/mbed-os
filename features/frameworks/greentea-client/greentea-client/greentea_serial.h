6d4848d35ab76ecb62bfa67e0196d0877f82403e
Gabi Sierp <Gabi.Sierp@rosenberger.com>
2018-05-25 12:38:31 +0200

    Patch for C-Spy simulation debugger added.

--- features/frameworks/greentea-client/greentea-client/greentea_serial.h
+++ features/frameworks/greentea-client/greentea-client/greentea_serial.h
@@ -7,10 +7,25 @@
 #include "RawSerial.h"
 #include "SingletonPtr.h"
 
+#ifdef	SIMULATION
+#include "LowLevelIOInterface.h"
+
+class GreenteaSerial
+{
+public:
+    GreenteaSerial();
+    int getc();
+    int putc(int cc);
+    int puts(const char *str);
+    int printf(const char *format, ...);
+};
+
+#else
 class GreenteaSerial : public mbed::RawSerial {
 public:
     GreenteaSerial();
 };
+#endif
 
 extern SingletonPtr<GreenteaSerial> greentea_serial;
 #endif
